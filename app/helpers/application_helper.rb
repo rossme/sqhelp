0452a15c52fc0f4f43b0fd47466cdd711f6f83bd
Ross Buddie <66185926+rossme@users.noreply.github.com>
2023-10-24 23:38:01 +0100

    Install octokit gem, add navbar, improve excersises (#10)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,27 @@ module ApplicationHelper
   def flash_class(level)
     FLASH_LEVELS[level&.to_sym].presence
   end
+
+  def github_release_details
+    # Check if the personal access token has expired, Octokit then raises an exception
+    return I18n.t('application.release_details.error') unless latest_release && latest_deploy
+
+    I18n.t('application.release_details.info', latest_release: latest_release, latest_deploy: latest_deploy)
+  end
+
+  def latest_release
+    Rails.cache.fetch('latest_release', expires_in: 12.hours) do
+      Octokit.latest_release('rossme/sqhelp')&.tag_name
+    end
+  rescue Octokit::Unauthorized, Octokit::TooManyRequests
+    nil # this error is handled in github_release_details
+  end
+
+  def latest_deploy
+    Rails.cache.fetch('latest_deploy', expires_in: 12.hours) do
+      Octokit.deployments('rossme/sqhelp').first[:updated_at]
+    end
+  rescue Octokit::Unauthorized, Octokit::TooManyRequests
+    nil # this error is handled in github_release_details
+  end
 end
