c4ba512c5b598275f5bb61d2cad3c1c8c1deb4bb
Ross Buddie <66185926+rossme@users.noreply.github.com>
2023-10-29 16:14:40 +0000

    Fixed Octokit issue and updated Gemfile (#11)

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem 'tzinfo-data', platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', require: false
 
+# Github API wrapper
+gem 'octokit', '~> 5.0'
+
 # Faker is a Ruby library for generating fake data
 gem 'faker'
 
@@ -63,6 +66,7 @@ gem 'jquery-rails'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[ mri mingw x64_mingw ]
+  gem 'dotenv-rails'
   gem 'pry-byebug'
   gem 'rspec-rails'
   gem 'rubocop'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,17 +18,19 @@ module ApplicationHelper
     I18n.t('application.release_details.info', latest_release: latest_release, latest_deploy: latest_deploy)
   end
 
+  # `rails dev:cache` toggles caching in development environment
   def latest_release
     Rails.cache.fetch('latest_release', expires_in: 12.hours) do
-      Octokit.latest_release('rossme/sqhelp')&.tag_name
+#       Octokit.latest_release('rossme/sqhelp')&.tag_name
     end
   rescue Octokit::Unauthorized, Octokit::TooManyRequests
     nil # this error is handled in github_release_details
   end
 
+  # `rails dev:cache` toggles caching in development environment
   def latest_deploy
     Rails.cache.fetch('latest_deploy', expires_in: 12.hours) do
-      Octokit.deployments('rossme/sqhelp').first[:updated_at]
+#       Octokit.deployments('rossme/sqhelp').first[:updated_at]
     end
   rescue Octokit::Unauthorized, Octokit::TooManyRequests
     nil # this error is handled in github_release_details
